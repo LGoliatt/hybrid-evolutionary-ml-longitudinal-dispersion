e7d9d9e6c11f538e072e2c73034693d6bf078935
Leonardo <goliatt@gmail.com>
2021-06-29 00:13:39 -0300

    goliatt ter jun 29 00:13:39 -03 2021

--- ldc_evoml_regression_v0p3.py
+++ ldc_evoml_regression_v0p3.py
@@ -20,12 +20,8 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
                                               ExpSineSquared, DotProduct,
                                               ConstantKernel)
-
-
-
-
 from read_data import *
-
+#%%----------------------------------------------------------------------------   
 program_name = sys.argv[0]
 arguments = sys.argv[1:]
 count = len(arguments)
